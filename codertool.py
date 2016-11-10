82440651d6c9c787b37bb5f39771a4b741f9b5a2
tong <jiantong912@163.com>
2016-11-10 16:21:54 +0800

    提交第一个功能

--- codertool.py
+++ codertool.py
@@ -6,7 +6,7 @@ Module implementing codertool.
 
 from PyQt5.QtCore import pyqtSlot
 from PyQt5.QtWidgets import QMainWindow, QMessageBox
-import urllib, urllib.parse, urllib.request, time, re, cgi
+import urllib, urllib.parse, urllib.request, time, re, cgi, datetime
 from Ui_codertool import Ui_codertool
 
 
@@ -23,7 +23,14 @@ class codertool(QMainWindow, Ui_codertool):
         """
         super(codertool, self).__init__(parent)
         self.setupUi(self)
-    
+        self.getNowtime()
+    #初始化信息
+    def getNowtime(self):
+        now_date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime());
+        now_timestamp = str(int(time.time()))
+        self.dateTime3.setText(now_date)
+        self.timestamp2.setText(now_timestamp)
+        return
     @pyqtSlot()
     def on_push_timetodate_clicked(self):
         #QMessageBox.information(self, '测试', '测试')
@@ -33,19 +40,35 @@ class codertool(QMainWindow, Ui_codertool):
         if from_timestamp=='':
             QMessageBox.information(self, '提示', '请填写时间戳！')
             return
-        QMessageBox.information(self, '提示', re.match('^[0-9]+$', from_timestamp));
-        if re.match('^[0-9]+$', from_timestamp)==None:
-            QMessageBox.information(self, '提示', '必须填写数字！')
+        regx = re.compile(r"^[0-9]{10}$");
+        if regx.match( from_timestamp)==None:
+            QMessageBox.information(self, '提示', '必须填写10位数字！')
             return
-        QMessageBox.information(self, '提示', from_timestamp)
+        #QMessageBox.information(self, '提示', from_timestamp)
+        todatetime = time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(int(from_timestamp)))
+        #QMessageBox.information(self, '提示', todatetime)
+        self.dateTime2.setText(todatetime)
         #raise NotImplementedError
+        return 
     
     @pyqtSlot()
     def on_push_datetotime_clicked(self):
         """
         Slot documentation goes here.
         """
-        #raise NotImplementedError
+        from_datetime = self.dateTime3.text().strip()
+        if from_datetime=='':
+            QMessageBox.information(self, '提示', '请填写日期格式！')
+            return
+        regx = re.compile(r"^[0-9]{4}\-[0-1][0-9]\-[0-3][0-9] [0-1][0-9]:[0-5][0-9]:[0-5][0-9]$");
+        if regx.match( from_datetime)==None:
+            QMessageBox.information(self, '提示', '必须填写2015-11-12 12:12:12格式的时间！')
+            return
+        date_tmp=datetime.datetime.strptime(str(from_datetime),"%Y-%m-%d %H:%M:%S")
+        to_timestamp = str(int(time.mktime(date_tmp.timetuple())))
+        self.timestamp3.setText(to_timestamp)
+        return
+
 
 
 if __name__ == '__main__':
